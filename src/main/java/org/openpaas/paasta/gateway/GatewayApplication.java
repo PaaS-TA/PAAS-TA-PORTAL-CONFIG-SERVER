5056022b735ecb86150d6207243a8e9ea3f69ad4
swmoon <swmoon@bluedigm.com>
2018-02-06 13:18:12 +0900

    PAAS-TA V2 - Application 파일 위치 및 이름 수정

--- src/main/java/org/openpaas/paasta/gateway/GatewayApplication.java
+++ src/main/java/org/openpaas/paasta/gateway/GatewayApplication.java
@@ -10,10 +10,10 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 
 @EnableConfigServer
 @SpringBootApplication
-public class ConfigApplication {
+public class GatewayApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ConfigApplication.class, args);
+        SpringApplication.run(GatewayApplication.class, args);
     }
 }
 
