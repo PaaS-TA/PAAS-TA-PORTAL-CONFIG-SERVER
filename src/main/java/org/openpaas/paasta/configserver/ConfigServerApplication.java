544fa439047bb4291c76ab1e0ed3086adc115769
swmoon <swmoon@bluedigm.com>
2018-06-21 18:55:47 +0900

    hytrix 적용 중

--- src/main/java/org/openpaas/paasta/configserver/ConfigServerApplication.java
+++ src/main/java/org/openpaas/paasta/configserver/ConfigServerApplication.java
@@ -3,11 +3,13 @@ package org.openpaas.paasta.configserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 /**
  * The type Application.
  */
 
+@EnableHystrix
 @EnableConfigServer
 @SpringBootApplication
 public class ConfigServerApplication {
